78112aa339868b57937c3f8ac73320c49013df24
Daniel Tate <danieltate888@hotmail.com>
2025-06-21 23:25:11 +0930

    Fix: Update password validation rules to ensure new password is different from current password

--- apps/Server/SmartRetail360.Application/Validators/Users/UpdateUserPasswordRequestValidator.cs
+++ apps/Server/SmartRetail360.Application/Validators/Users/UpdateUserPasswordRequestValidator.cs
@@ -3,15 +3,14 @@ using SmartRetail360.Application.Models;
 using SmartRetail360.Contracts.Users.Requests;
 using SmartRetail360.Shared.Enums;
 
-namespace SmartRetail360.Application.Validators.User;
+namespace SmartRetail360.Application.Validators.Users;
 
 public class UpdateUserPasswordRequestValidator: AbstractValidator<UpdateUserPasswordRequest>
 {
     public UpdateUserPasswordRequestValidator(ApplicationDependencies dep)
     {
         RuleFor(x => x.CurrentPassword)
-            .NotEmpty().WithMessage(dep.Localizer.GetLocalizedText(LocalizedTextKey.CurrentPasswordIsRequired))
-            .EmailAddress().WithMessage(dep.Localizer.GetLocalizedText(LocalizedTextKey.InvalidEmailFormat));
+            .NotEmpty().WithMessage(dep.Localizer.GetLocalizedText(LocalizedTextKey.CurrentPasswordIsRequired));
 
         RuleFor(x => x.NewPassword)
             .NotEmpty().WithMessage(dep.Localizer.GetLocalizedText(LocalizedTextKey.NewPasswordIsRequired))
@@ -22,7 +21,7 @@ public class UpdateUserPasswordRequestValidator: AbstractValidator<UpdateUserPas
             .Matches("[0-9]").WithMessage(dep.Localizer.GetLocalizedText(LocalizedTextKey.PasswordMustContainDigit))
             .Matches("[^a-zA-Z0-9]")
             .WithMessage(dep.Localizer.GetLocalizedText(LocalizedTextKey.PasswordMustContainSpecialCharacter))
-            .Equal(x => x.CurrentPassword)
+            .NotEqual(x => x.CurrentPassword)
             .WithMessage(dep.Localizer.GetLocalizedText(LocalizedTextKey.PasswordMustBeDifferentFromCurrent));
         
         RuleFor(x => x.NewPasswordConfirmation)
