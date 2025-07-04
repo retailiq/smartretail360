1a8350ed962445f83e4580d936e5f282910d088c
Daniel Tate <daniel@danieltate.io>
2025-07-04 15:22:49 +0930

    Update: Refactor Docker configuration and add CI/CD pipeline

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,8 +1,14 @@
 
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  assetPrefix: "/client",
+  ...(isProd && {
+    basePath: "/client",
+    assetPrefix: "/client",
+  }),
+  output: "standalone",
 };
 
 export default nextConfig;
